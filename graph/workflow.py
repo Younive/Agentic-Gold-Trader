02c9682bf6ff7dc9a5384fc4a687a415a17bdb99
Siriwut <67328603+Younive@users.noreply.github.com>
2025-07-14 19:28:26 +0700

    fix: import package properly

--- graph/workflow.py
+++ graph/workflow.py
@@ -1,11 +1,11 @@
 from langgraph.graph import StateGraph, END
-from langgraph.graph.state import AgentState
+from src.models import AgentState
 from agent.fundamental_agent import fundamental_analyst_agent
 from agent.technical_analyst_agent import technical_analyst_agent
 from agent.market_sentiment_agent import market_sentiment_analyst_agent
 from agent.oi_analyst_agent import oi_analyst_agent
 from agent.retail_sentiment_agent import retail_sentiment_agent
-from agent.strategist_agent import chief_strategist_agent
+from agent.strategist_agent import strategist_agent
 
 # Define the workflow graph
 workflow = StateGraph(AgentState)
@@ -15,8 +15,8 @@ workflow.add_node("fundamental_analyst", fundamental_analyst_agent)
 workflow.add_node("technical_analyst", technical_analyst_agent)
 workflow.add_node("sentiment_analyst", market_sentiment_analyst_agent)
 workflow.add_node("oi_analyst", oi_analyst_agent)
-workflow.add_node("retail_sentiment_agent", retail_sentiment_agent) # <-- ADD NEW NODE
-workflow.add_node("chief_strategist", chief_strategist_agent)
+workflow.add_node("retail_sentiment_agent", retail_sentiment_agent)
+workflow.add_node("chief_strategist", strategist_agent)
 
 # Define the edges
 workflow.set_entry_point("fundamental_analyst")

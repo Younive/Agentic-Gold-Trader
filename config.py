f44fd9601bbf098464a32d4344a7501371640bb5
Siriwut <67328603+Younive@users.noreply.github.com>
2025-12-17 16:17:24 +0700

    modified: added RAG related setting

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
 from firecrawl import FirecrawlApp
 
 # Load environment variables from .env file
@@ -11,3 +11,14 @@ llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash-lite")
 
 # Initialize the Firecrawl client
 firecrawl_app = FirecrawlApp(api_key=os.environ["FIRECRAWL_API_KEY"])
+
+# Vector DB configuration
+VECTOR_DB_DIR = os.path.join(os.path.dirname(__file__), "trading_journal_rag", "vector_db")
+
+# Document Preprocessing
+KNOWLEDGE_BASE_DIR = os.path.join(os.path.dirname(__file__), "trading_journal_rag", "knowledge_base")
+CHUNK_SIZE = 1000
+CHUNK_OVERLAP = 200
+
+# Initialize embedding model
+embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
